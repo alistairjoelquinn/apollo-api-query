9da8a7076c612396b3bb650658dfaa4d9b1dfcc5
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-07-30 14:13:53 +0200

    removes uneccesary imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient, { gql } from 'apollo-boost';
-import BookInfo from '@/components/BookInfo';
 
 const Home = ({ data }) => {
     const client = new ApolloClient({
@@ -11,7 +10,6 @@ const Home = ({ data }) => {
         <ApolloProvider client={client}>
             <div>
                 <h1>NextJS GraphQL Apollo App</h1>
-                <BookInfo />
             </div>
         </ApolloProvider>
     );
